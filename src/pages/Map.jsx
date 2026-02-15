e1bb9b23f967ee28d574ad5660304d03342f5d6e
LoZiodelRum <mauriziograci@gmail.com>
2026-02-15 23:42:34 +0100

    mappa chiara voyager senza stelline

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -100,7 +100,7 @@ export default function MapPage() {
         <ZoomControl position="bottomleft" />
         <TileLayer
   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-  url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+  url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
 />
         {filteredVenues.map((venue) => (
           <Marker
