e8aee55e98a442c51687a660278e560c4016f558
LoZiodelRum <mauriziograci@gmail.com>
2026-02-15 23:37:08 +0100

    nuova mappa senza stelline

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -99,9 +99,9 @@ export default function MapPage() {
       >
         <ZoomControl position="bottomleft" />
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+  url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+/>
         {filteredVenues.map((venue) => (
           <Marker
             key={venue.id}
