26bcb5d40da091ab6bc43180d6b8722eba606791
LoZiodelRum <mauriziograci@gmail.com>
2026-04-05 20:48:10 +0200

    Mobile: testo corpo articolo a 18px

--- src/pages/ArticleDetail.tsx
+++ src/pages/ArticleDetail.tsx
@@ -240,12 +240,12 @@ export default function ArticleDetail() {
           }
 
           .article-content {
-            font-size: 19px !important;
+            font-size: 18px !important;
           }
           .article-content p,
           .article-content span,
           .article-content li {
-            font-size: 19px !important;
+            font-size: 18px !important;
           }
         }
       `}</style>
