eae13d5a4ea5ce89c31ce0a955940427b23685fc
LoZiodelRum <mauriziograci@gmail.com>
2026-04-15 15:07:57 +0200

    Restore single hero video (remove second clip)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,7 +20,7 @@ type Articolo = {
   immagine: string | null;
 };
 
-const HOME_HERO_VIDEO_VERSION = "2026-04-15-04";
+const HOME_HERO_VIDEO_VERSION = "2026-04-15-05";
 
 export default function Home() {
   const { isAdmin } = useUser();
