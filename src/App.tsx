7399c50188c8a99bab6baaea9a0908943d040ee9
LoZiodelRum <mauriziograci@gmail.com>
2026-04-24 00:53:49 +0200

    fix: usa Auth.tsx come pagina principale (login moderno, robusto, con Navbar)

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
 import Home from "./pages/Home";
-import Login from "./pages/Login";
+import Auth from "./pages/Auth";
 import ProtectedRoute from "./pages/ProtectedRoute";
 
-export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Auth />} />
       <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
       {/* altre rotte protette: <Route path="/altra" element={<ProtectedRoute><Altra /></ProtectedRoute>} /> */}
     </Routes>
