16f7cdcb2b9e6b62a54d3a0594d5b631b3b7febd
LoZiodelRum <mauriziograci@gmail.com>
2026-04-24 00:56:40 +0200

    fix: aggiunta dichiarazione corretta funzione App in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-//
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import ProtectedRoute from "./pages/ProtectedRoute";
 
+export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Auth />} />
