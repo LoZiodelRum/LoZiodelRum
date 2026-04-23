0e1f4422130401ba5003a3aa4a3bbf686fd64858
LoZiodelRum <mauriziograci@gmail.com>
2026-04-23 23:39:38 +0200

    Fix: aggiunto BrowserRouter per routing React Router

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
 import "./index.css";
 
@@ -28,9 +29,11 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <BrowserRouter>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
 );
