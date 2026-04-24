3205f1a5a1c273b70f2f5b2fe64d11fbf8ece686
LoZiodelRum <mauriziograci@gmail.com>
2026-04-24 20:59:21 +0200

    fix(RegisterModal): chiusura corretta JSX e struttura, fix errori build TS17008 TS1381 TS1005

--- src/components/RegisterModal.tsx
+++ src/components/RegisterModal.tsx
@@ -267,7 +267,8 @@ export default function RegisterModal({ open, onClose }: Props) {
         {msg && (
           <div style={{ color: "#FFD36A", marginTop: 16, textAlign: "center" }}>{msg}</div>
         )}
-      </div>
+        </div> {/* chiusura .register-modal-box */}
+      </div> {/* chiusura box centrale */}
     </div>
   );
 }
