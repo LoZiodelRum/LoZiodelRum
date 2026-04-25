ee6cbdd6b2c908d22f95e8bfbb11d759382cd0de
LoZiodelRum <mauriziograci@gmail.com>
2026-04-25 11:28:35 +0200

    fix: rimosso shadowUrl da mergeOptions per compatibilità marker Leaflet

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -22,8 +22,7 @@ const customIcon = L.icon({
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconUrl: "/marker.png",
-  iconRetinaUrl: "/marker.png",
-  shadowUrl: ""
+  iconRetinaUrl: "/marker.png"
 });
 
 type Locale = {
