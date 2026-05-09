35557ef7845b3bbdf85fd3b80c5bceb8b4900f42
LoZiodelRum <mauriziograci@gmail.com>
2026-05-10 01:07:22 +0200

    Fix AdminPanel responsive layout and restore desktop/mobile rendering

--- src/pages/AdminPanel.tsx
+++ src/pages/AdminPanel.tsx
@@ -9,18 +9,21 @@ export default function AdminPanel() {
   useEffect(() => {
     const mediaQuery = window.matchMedia("(max-width: 1023px)");
 
-    const handleChange = () => {
-      setIsMobile(mediaQuery.matches);
+    const handleChange = (e?: MediaQueryListEvent) => {
+      setIsMobile(e ? e.matches : mediaQuery.matches);
     };
 
+    // inizializzazione
     handleChange();
 
+    // listener responsive
     if (mediaQuery.addEventListener) {
       mediaQuery.addEventListener("change", handleChange);
     } else {
       mediaQuery.addListener(handleChange);
     }
 
+    // cleanup corretto
     return () => {
       if (mediaQuery.removeEventListener) {
         mediaQuery.removeEventListener("change", handleChange);
@@ -30,16 +33,15 @@ export default function AdminPanel() {
     };
   }, []);
 
-    return (
-      <div
-        style={{
-          minHeight: "100vh",
-          background: "#020B1C",
-          color: "white",
-          paddingTop: "140px",
-        }}
-      >
-        ADMIN PANEL TEST OK
+  return (
+    <div className="min-h-screen bg-[#020B1C]">
+      {/* Navbar ufficiale */}
+      <Navbar />
+
+      {/* Contenuto admin */}
+      <div className="pt-[110px]">
+        {isMobile ? <AdminPanelMobile /> : <AdminPanelDesktop />}
       </div>
-    );
+    </div>
+  );
 }
