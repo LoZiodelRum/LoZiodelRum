e8fd36158cf552782548cda8ca80092a8d24e182
LoZiodelRum <mauriziograci@gmail.com>
2026-05-21 23:50:09 +0200

    style: added safe global page spacing

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,15 +3,10 @@ import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
-  // Responsive padding-top: 95px mobile, 110px desktop
-  // Navbar fixed, main sempre con padding-top
   return (
     <div className="min-h-screen bg-[#020B1C] overflow-x-hidden">
       <Navbar />
-      <main
-        className="pt-[95px] md:pt-[110px]"
-        style={{ minHeight: "calc(100vh - 95px)" }}
-      >
+      <main className="pt-6 md:pt-8">
         <Outlet />
       </main>
     </div>
