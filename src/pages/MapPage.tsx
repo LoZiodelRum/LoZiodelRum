40add8ab374954eeb305c101986487a08002e7d9
LoZiodelRum <mauriziograci@gmail.com>
2026-05-21 23:41:16 +0200

    fix: restored leaflet marker icons

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -3,11 +3,21 @@ import { MapContainer, TileLayer, Marker, ZoomControl } from "react-leaflet";
 import { X } from "lucide-react";
 import { supabase } from "../lib/supabaseClient";
 import Navbar from "../components/Navbar";
-import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 // 🔧 FIX fondamentale Leaflet
 delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 // ✅ MARKER PERSONALIZZATO
 const customIcon = L.icon({
